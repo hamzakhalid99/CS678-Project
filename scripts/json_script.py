25c4fb0e1a5f2bcee4d81acd4a1b94819e406398
hamzakhalid99 <hamza.k.malik@outlook.com>
2021-03-29 05:16:53 +0500

    gradient1

--- scripts/json_script.py
+++ scripts/json_script.py
@@ -3,7 +3,8 @@ import os
 import csv
 import matplotlib.pyplot as plt
 import random
-
+import pprint
+import numpy as np
 
 '''
 {
@@ -22,6 +23,9 @@ import random
 }
 '''
 
+
+
+#not being used
 def greatestImpact(audits, metricOne, metricTwo):
     # print(audits)
     impact = {}
@@ -30,14 +34,13 @@ def greatestImpact(audits, metricOne, metricTwo):
         binary_scores = scores_dict["binary_score_audits"]
         null_scores = scores_dict["null_score_audits"]
         impact[website] = {} 
-
         for  metric, details in numeric_scores.items(): #metric is what is being scored (e.g. unused-javascript), while details include scoring detailsm overall savings etc. 
             try:
                 impact[website][metric] = [details["score"], details["overallSavingsMs"], details["numericValue"], details["numericUnit"]]   #format: website -> [score, savings]
             except:
-                print(metric)
+                pass
         for  metric, details in binary_scores.items(): #metric is what is being scored (e.g. unused-javascript), while details include scoring detailsm overall savings etc. 
-            print("here", metric)
+            # print("here", metric)
             try:
                 impact[website][metric] = [details["score"], details["overallSavingsMs"], details["numericValue"], details["numericUnit"]]   #format: website -> [score, savings]
             except:
@@ -47,7 +50,7 @@ def greatestImpact(audits, metricOne, metricTwo):
                 impact[website][metric] = [details["score"], details["overallSavingsMs"], details["numericValue"], details["numericUnit"]]   #format: website -> [score, savings]
             except:
                 pass
-    print(impact["20-netflix.com"]["unused-javascript"])
+    # print(impact["20-netflix.com"]["unused-javascript"])
     
     metricOneValues = []
     metricTwoValues = []
@@ -61,30 +64,33 @@ def greatestImpact(audits, metricOne, metricTwo):
             elif key == metricTwo:
                 metricTwoValues.append(impact[website][key][2])
     
-    print (metricOneValues, metricTwoValues)
+    # print (metricOneValues, metricTwoValues)
 
     y_axis = list(range(1,len(metricOneValues)+1))
 
-    # plotGraph (y_axis, metricOneValues, metricOne+" vs "+metricTwo, "Rank of website", "Average savings", metricOne+"_vs_"+metricTwo)
-    # plotGraph (y_axis, metricTwoValues, metricOne+" vs "+metricTwo, "Rank of website", "Average savings", metricOne+"_vs_"+metricTwo)
+    plotGraph (y_axis, metricOneValues, metricOne+" vs "+metricTwo, "Rank of website", "Average savings", metricOne+"_vs_"+metricTwo)
+    plotGraph (y_axis, metricTwoValues, metricOne+" vs "+metricTwo, "Rank of website", "Average savings", metricOne+"_vs_"+metricTwo)
     # plt.show()
 
 def makeAuditList ():
     audit_obj = {}
-    for file_name in os.listdir("data/json_files"):
+    for file_name in os.listdir("../data/json_files"):
         file_name = file_name[:-5].lower()
         audit_obj[file_name] = {
             "numeric_score_audits": {},
             "binary_score_audits": {},
             "null_score_audits": {}
         }
-    for file_name in sorted(os.listdir("data/json_files"), key=lambda x: int(x.partition('-')[0])):
-        with open("data/json_files/" + file_name) as json_file:
+    for file_name in sorted(os.listdir("../data/json_files"), key=lambda x: int(x.partition('-')[0])):
+        with open("../data/json_files/" + file_name) as json_file:
             # file_name = file_name.split('.')[0].lower()
             file_name = file_name[:-5].lower()
             json_file_obj = json.load(json_file)
             audits = json_file_obj["audits"]
+            # print(audits['largest-contentful-paint'])
             for key, value in audits.items():
+                # if key == 'largest-contentful-paint':
+                #     print(file_name)
                 if value["scoreDisplayMode"] == "numeric":
                     if "details" in value and "numericValue" in value:
                         if "overallSavingsMs" in value["details"]:
@@ -103,7 +109,8 @@ def makeAuditList ():
                                         "overallSavingsMs": value["details"]["overallSavingsMs"]
                                     }})
 
-                    elif "details" not in value and "numericValue" in value:
+                    elif "details" not in value and ("numericValue" in value):
+                        # print('here')
                         audit_obj[file_name]['numeric_score_audits'].update({\
                                 value["id"]: {
                                     "score": value["score"],
@@ -130,7 +137,7 @@ def makeAuditList ():
                                     "score": value["score"],
                                 }})
 
-                if value["score"] == None:
+                elif value["score"] == None:
                     if "numericValue" in value:
                         audit_obj[file_name]['null_score_audits'].update({\
                                 value["id"]: {
@@ -138,6 +145,7 @@ def makeAuditList ():
                                     "numericValue": value["numericValue"],
                                     "numericUnit": value["numericUnit"]
                                 }})
+
                     else:
                         audit_obj[file_name]["null_score_audits"].update({\
                                 value["id"]: {
@@ -145,31 +153,24 @@ def makeAuditList ():
                                 }})
     return audit_obj
 
-def plotGraph (x, y, title_, xlabel_, ylabel_, name):
-    rgb = (random.random(), random.random(), random.random())
-    plt.plot(x, y, c=rgb)
-    plt.title(title_)
-    plt.xlabel(xlabel_)
-    plt.ylabel(ylabel_)
-    # plt.show()
-    plt.savefig(name)
+
 
 def auditScorevsTime (audits):
     for key, value in audits.items():
         scorePoints = []
         savingsPoints = []
-        i = 0
-        if i < 10:
-            for key2, value2 in value['numeric_score_audits'].items():
-                if "overallSavingsMs" in value2:
-                    scorePoints.append(value2['score'])
-                    savingsPoints.append(value2['overallSavingsMs'])
-        else:
-            break
-        plotGraph(scorePoints, savingsPoints, 'Score vs time_savings', 'score', 'potential saved time in ms', 'score-vs-overallSavingsMs')
+        # i = 0
+        # if i < 10:
+        for key2, value2 in value['numeric_score_audits'].items():
+            if "overallSavingsMs" in value2:
+                scorePoints.append(value2['score'])
+                savingsPoints.append(value2['overallSavingsMs'])
+        # else:
+        #     break
         # break
-        i += 1
-    plt.show()
+        # i += 1
+    plotGraph(scorePoints, savingsPoints, 'Score vs time_savings', 'score', 'potential saved time in ms', 'score-vs-overallSavingsMs')
+    # plt.show()
 
 def specificAuditScoreTrend (auditName, audits):
     scorePoints = []
@@ -184,6 +185,8 @@ def specificAuditScoreTrend (auditName, audits):
     plotGraph(x, scorePoints, "Ranking-vs-" + auditName, "Ranking", "Score: " + auditName, "ranking-vs-" + auditName)
 
 
+
+#not being used
 def readFile (fileName, websiteType, audits):
     filtered = {
         'e': [],
@@ -203,25 +206,83 @@ def readFile (fileName, websiteType, audits):
                     filtered['s'].append(row[0] + '-' + row[1])
             i += 1
         return filtered
+
+
+def performanceMetricAnalysis (audits, metricOne, metricTwo):
+    metricOneNumericVals = []
+    metricOneScores = []
+    metricTwoNumericVals = []
+    metricTwoSCores = []
+    for key, value in audits.items():
+        for key2, value2 in value['numeric_score_audits'].items():
+            if key2 == metricTwo:
+                if 'score' in value2:
+                    metricTwoSCores.append(value2['score'])
+                if 'numericValue' in value2:
+                    metricTwoNumericVals.append(value2['numericValue'])
+            if key2 == metricOne:
+                if 'score' in value2:
+                    metricOneScores.append(value2['score'])
+                if 'numericValue' in value2:
+                    metricOneNumericVals.append(value2['numericValue'])
+    plotGraph(metricOneScores, metricTwoNumericVals, metricOne + " score vs " + metricTwo + " numeric Value", metricOne + " scores", metricTwo + ' numericValue', metricOne + " score vs " + metricTwo + " numeric Value")
+
+def plotGraph (x, y, title_, xlabel_, ylabel_, name):
+    rgb = (random.random(), random.random(), random.random())
+    x = np.array(x)
+    y = np.array(y)
+    m, b = np.polyfit(x, y, 1)
+    plt.plot(x, m*x + b, c=rgb)
+    plt.title(title_)
+    plt.xlabel(xlabel_)
+    # plt.set_label(legend_)
+    plt.ylabel(ylabel_)
+    # plt.legend()
+    # plt.savefig(name)
+    # plt.show()
+
+def overallMetricAnalysis (audits, metric):
+    scores = {}
+    numericValues = []
+    for key, value in audits.items():
+        for key2, value2 in value['numeric_score_audits'].items():
+            if 'score' in value2:
+                scores[key2] = []
+            if key2 == metric:
+                numericValues.append(value2['numericValue'])
+
+    for key, value in audits.items():
+        for key2, value2 in value['numeric_score_audits'].items():
+            if 'score' in value2:
+                scores[key2].append(value2['score'])
+            else:
+                print(key)
+
+    # print(len(scores['preload-lcp-image']))
+    for key, value in audits.items():
+        for key2, value2 in value['numeric_score_audits'].items():
+            # if len(numericValues) != len(scores[key2]):
+            #     print(key2, key)
+            # print()
+            try:
+                plotGraph(scores[key2], numericValues, metric + " numeric Value vs diagnostic results scores", 'score' ,  metric \
+                    + " numeric Values", 'effect of different diagnostics on ' + metric)
+                
+            except:
+                # print('here')
+                pass
+                # print(key2, key)
+            # pass
+    # plt.legend()
+    # plt.show()
+    plt.savefig('plot_overall')
+    # print(scores)
 if __name__ == "__main__":
-    audits = makeAuditList()
-    # print(audits['20-netflix.com'])
-    greatestImpact(audits, "largest-contentful-paint", "unused-javascript")
-    # specificAuditScoreTrend("unused-javascript", audits)
-    # filtered = readFile("data/categories.csv", 'r', audits)
-    # print(audits['01-google.com'])
-    # newAudits = {}
-    # i = 0
-    # for key, value in filtered.items():
-    #     print(value)
-    #     for website in value:
-    #         if i < 50:
-    #             newAudits[website] = audits[website]
-    #         else:
-    #             break
-    #     i += 1
-    # print(newAudits)
-    # print(audits['google']['numeric_score_audits'])
+    audits = makeAuditList()        
+    # performanceMetricAnalysis (audits, 'unused-javascript', "largest-contentful-paint")
+    # performanceMetricAnalysis (audits, 'unused-css-rules', "largest-contentful-paint")
+    # performanceMetricAnalysis (audits, 'unused-css-rules', "first-contentful-paint")
+    # print(audits)
+    overallMetricAnalysis(audits, 'largest-contentful-paint')
+    # specificAuditScoreTrend("unused-css-rules", audits)
     # auditScorevsTime(audits)
-    # print(greatestImpact(audits))
-    # print (audits["39-adobe.com"]['numeric_score_audits']["unused-javascript"])
